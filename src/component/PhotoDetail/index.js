a9a964bef15905155d2d0465f1fb1ddb3f5239e5
WEITAO JIANG <weitaoj@outlook.com>
2022-07-27 19:24:04 +1000

    Update index.js

--- src/component/PhotoDetail/index.js
+++ src/component/PhotoDetail/index.js
@@ -1,21 +1,14 @@
-import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-
-const PhotoDetail = ({ id, title, owner, secret, server_id }) => {
-  let params = useParams();
-
-  useEffect(() => {
-    const imgURL = `https://live.staticflickr.com/${server_id}/${id}_${secret}_b.jpg`;
-    return (
-      <div className="FilmDetail is-hydrated">
-        <figure className="film-backdrop">
-          <img src={imgURL} alt={title} />
-          <h1 className="film-title">{title}</h1>
-          <h2 className="film-title">{owner}</h2>
-        </figure>
-      </div>
-    );
-  }, [params.photoId]);
-};
+function PhotoDetail({ id, title, owner, secret, server_id }) {
+  const imgURL = `https://live.staticflickr.com/${server_id}/${id}_${secret}_b.jpg`;
+  return (
+    <div className="FilmDetail is-hydrated">
+      <figure className="film-backdrop">
+        <img src={imgURL} alt={title} />
+        <h1 className="film-title">{title}</h1>
+        <h2 className="film-title">{owner}</h2>
+      </figure>
+    </div>
+  );
+}
 
 export default PhotoDetail;
